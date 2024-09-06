3b68d508fb55ecd20de5a436589dd0a8ef3877c5
Debora-Carvalho <deboravolc2@gmail.com>
2024-09-05 22:29:21 -0300

    iniciar o repositório e adicionar arquivos do projeto

---
+++ dados.js
@@ -0,0 +1,18 @@
+let listDados = [
+{    
+    titulo: "Rainha das Lágrimas",
+    descricao: "Rainha das Lágrimas é um drama coreano que acompanha a história de um casal de magnatas, Hae-in e Hyun-woo, que se apaixonam na juventude e se casam. No entanto, com o passar dos anos, o casal enfrenta uma crise matrimonial profunda, enquanto navegam pelos desafios do mundo corporativo e das intrigas familiares. A série explora o amor, a traição e a busca pela felicidade em um ambiente de poder e riqueza.",
+    link: "#"
+},
+{
+    titulo: "Tudo bem não ser normal",
+    descricao: "Tudo bem não ser normal é um drama coreano que explora a saúde mental e as relações humanas através da história de um escritor de livros infantis e um cuidador de hospital psiquiátrico. A série aborda temas como trauma, solidão e a importância da conexão humana.",
+    link: "#"
+},
+{
+    titulo: "The Good Bad Mother",
+    descricao: "The Good Bad Mother é um drama coreano que conta a história de uma mãe que tenta reconectar-se com seu filho adulto, após um trágico acidente que o faz voltar à infância. A série explora temas como amor maternal, culpa e a importância da família.",
+    link: "#"
+}
+];
+
