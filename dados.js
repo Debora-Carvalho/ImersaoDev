cc6aac86b8b55dacec316eb93e71b5a92295b770
Debora-Carvalho <deboravolc2@gmail.com>
2024-09-05 22:56:11 -0300

    incrementando a base de dados

--- dados.js
+++ dados.js
@@ -13,6 +13,51 @@ let listDados = [
     titulo: "The Good Bad Mother",
     descricao: "The Good Bad Mother é um drama coreano que conta a história de uma mãe que tenta reconectar-se com seu filho adulto, após um trágico acidente que o faz voltar à infância. A série explora temas como amor maternal, culpa e a importância da família.",
     link: "#"
+},
+{
+    titulo: "Vincenzo",
+    descricao: "Um conselheiro jurídico da máfia italiana retorna à Coreia do Sul para recuperar o ouro escondido e se envolve em um caso de vingança.",
+    link: "#"
+},
+{
+    titulo: "Start-Up",
+    descricao: "Um grupo de jovens empreendedores luta para ter sucesso no mundo das startups, enquanto se envolvem em um triângulo amoroso.",
+    link: "#"
+},
+{
+    titulo: "Hometown Cha-Cha-Cha",
+    descricao: "Um dentista da cidade se muda para uma pequena vila à beira-mar e se apaixona por um homem de faz-tudo local, com uma vida misteriosa.",
+    link: "#"
+},
+{
+    titulo: "Nosso eterno verão",
+    descricao: "Cinco amigos de infância se reencontram após uma década e revivem memórias do passado, enquanto lidam com os desafios da vida adulta.",
+    link: "#"
+},
+{
+    titulo: "Navillera",
+    descricao: "Um homem de 70 anos descobre o balé e se esforça para realizar seu sonho de infância, enquanto um jovem bailarino o ajuda a superar seus desafios.",
+    link: "#"
+},
+{
+    titulo: "Pousando no amor",
+    descricao: "Uma rica herdeira sul-coreana faz um pouso de emergência na Coreia do Norte durante um voo de parapente e se apaixona por um oficial do exército norte-coreano.",
+    link: "#" 
+},
+{
+    titulo: "Beleza Verdadeira",
+    descricao: "Uma mulher com baixa autoestima se transforma em uma beleza após uma cirurgia plástica e descobre o verdadeiro significado da beleza.",
+    link: "#"
+},
+{
+    titulo: "Pretendente surpresa",
+    descricao: "Essa comédia romântica gira em torno de Shin Ha-ri, uma funcionária que se oferece para ir a um encontro às cegas no lugar de sua amiga. No entanto, ela não esperava que o encontro fosse com o CEO da empresa onde trabalha, Kang Tae-mu. Para evitar mais complicações, Ha-ri inventa uma persona completamente diferente, se passando por uma mulher rica e sofisticada. O que começa como uma mentira inocente se transforma em um jogo de gato e rato, com os dois protagonistas se apaixonando cada vez mais um pelo outro.",
+    link: "#"
+},
+{
+    titulo: "Sorriso Real",
+    descricao: "Sorriso real é um drama romântico sul-coreano que conta a história de um encontro improvável entre dois mundos completamente diferentes: o de Goo Won, um herdeiro arrogante e cínico de uma poderosa rede hoteleira, e Cheon Sa-rang, uma funcionária dedicada e otimista do mesmo hotel.",
+    link: "#"
 }
 ];
 
